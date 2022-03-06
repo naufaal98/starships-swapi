df6c9d39e8b5e3e752b977b2f3b0ea3bb508c3ea
Muhammad Naufal <muhammad_naufal@hotmail.com>
2022-03-06 20:30:57 +0700

    chore: change page extensions rule

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ['page.tsx', 'page.ts', 'api.ts'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
